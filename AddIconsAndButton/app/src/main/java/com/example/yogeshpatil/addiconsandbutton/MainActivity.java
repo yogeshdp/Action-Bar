2b64eddca6979bf578f3e866b20fab585b82fa58
yogeshdp <pyogesh092@gmail.com>
2016-08-16 14:44:29 +0530

    Action bat click event add

--- AddIconsAndButton/app/src/main/java/com/example/yogeshpatil/addiconsandbutton/MainActivity.java
+++ AddIconsAndButton/app/src/main/java/com/example/yogeshpatil/addiconsandbutton/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.yogeshpatil.addiconsandbutton;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.settings){
+
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_launcher)
+                    .setTitle("")
+                    .setMessage("Settings")
+
+                    .show();
+        }
+
+        else if (id == R.id.about_us_id){
+
+            new AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_launcher)
+                    .setTitle("")
+                    .setMessage("About Us")
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void openHome(View view){
